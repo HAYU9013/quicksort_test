84d26b782cd22db8f643a963a31651eb4a3b50ff
HAYU <114861748+HAYU9013@users.noreply.github.com>
2025-07-29 13:04:38 +0000

    add code

---
+++ quick_sort.py
@@ -0,0 +1,12 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    left = [x for x in arr[1:] if x <= pivot]
+    right = [x for x in arr[1:] if x > pivot]
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+# 範例使用
+data = [5, 3, 8, 4, 2, 7, 1, 10]
+sorted_data = quick_sort(data)
+print("排序後結果：", sorted_data)
